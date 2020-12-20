6a46f989c0918a24ca7ffd1790a6ff6688a181af
Duxon5 <sigajunior@hotmail.com>
2020-12-20 13:46:09 -0300

    Realizado aperfeiçoamento na escolha
    O arquivo "teste" contém o melhoramento na escolha do arquivo. Ainda não está funcional via arquivo, porém está funcionando via String Estática.

--- teste.cpp
+++ teste.cpp
@@ -1,21 +1,45 @@
+// string::substr
 #include <iostream>
-#include <stdlib.h>
-
+#include <string>
+#include <sstream>
 using namespace std;
 
-
-
-int main(){
-	int x = 5;
-	int *p = &x;
+std::string * retorna(){
+	std::string palavras[5] = {"oie", "eaei mano", "beleza?", "bao?", "teste"};
+	std::string palavras_op[5];
 	
-	x = x + 4;
-	cout << "X: " << x << endl;
-	x = 5;
-	x = *p + 4;
-	cout << "X: " << x << endl;
-	x = 5;
-	*p = *p + 4;
-	cout << "X: " << p << endl;
+	std::stringstream str_bkp;
+	std::string out_string;
+	static string concat[5];
 	
+	for(int i = 0; i < 5; i++){
+		//std::cout << i + 1 << " - " << palavras[i] << "\n";
+		std::stringstream str;
+		str << i+1;
+		out_string = str.str();
+		//std::cout << "teste: " << out_string << "\t";
+		concat[i] = out_string + " - " + palavras[i];
+		cout << concat[i] << endl;
+		
+	}
+	return concat;
+}
+
+int main (){
+	std::string* ret_palavras;
+	
+	ret_palavras = retorna();
+	string valor;
+	cout << "\nDigite: ";
+	cin >> valor;
+	std::string escolhido;
+	
+	for(int i = 0; i < 5; i++){
+		string fPos = ret_palavras[i].substr(0,1);
+		if(valor == fPos){
+			escolhido = ret_palavras[i];
+			break;
+		}
+	}
+	cout << "resultado: " << escolhido;
 }
--- teste_salvar_arquivo.cpp
+++ teste_salvar_arquivo.cpp
@@ -6,6 +6,7 @@
 #include <fstream>
 #include <locale.h>
 #include <ctime>
+#include <string>
 
 using namespace std;
 void sleepcp(int milliseconds);
@@ -15,17 +16,21 @@ void sleepcp(int milliseconds){
     time_end = clock() + milliseconds * CLOCKS_PER_SEC/1000;
     while (clock() < time_end){}
 }
-void list_dir(const char *path) {
+
+int list_dir() {
+	//colocar a variável abaixo na função, se quiser chamar pelo main.
+	const char *path = "./";
+	
 	struct dirent *entry;
 	DIR *dir = opendir(path);
 	
 	if (dir == NULL) {
-		return;
+		return 0;
 	}
 	
 	cout << "Arquivos encontrados na pasta atual: " << endl;
 	
-	int arq_escolha = 1;
+	int arq_escolha = 0;
 	
 	while ((entry = readdir(dir)) != NULL) {
 		string tipo = entry->d_name;
@@ -34,41 +39,48 @@ void list_dir(const char *path) {
 		//cout << extensao << endl;
 		if(extensao == "txt"){
 			//sleepcp(250);
-			cout << arq_escolha << " - ";
+			cout << arq_escolha + 1 << " - ";
 			arq_escolha += 1;
 			cout << entry->d_name << endl;
 		}
 	}
 	closedir(dir);
+	
+	return arq_escolha;
 }
 
-char list_file_content(){
-	char filename[50];
+char list_file_content(int qtdArq){
+	char filename[1000];
 	ifstream file;
-	string line;
+	
 	//sleepcp(1500);
 	cout << "\nQual arquivo será aberto?" << endl;
-	cin.getline(filename, 50);
-	file.open(filename);
+	cin.getline(filename, 1000);
+	char firstPosition = filename[0];
 	
+	/*if(firstPosition == filename[0]){
+		std::cout << "Igual";
+		file.open(filename);
+	}*/
+	file.open(filename);
 	if(!file.is_open()){
 		exit(EXIT_FAILURE);
 	}
 	
-	char word[50];
-		
+	char word[1000];
+	string line;
+	
 	file >> word;
 	while(file.good()){
-		cout << word;
-		//cout << break_line << "\n";
 		file >> word;
+		cout << word;
 	}
 }
 
 int main() {
 	setlocale (LC_ALL,"");
 	
-	list_dir("./");	
-	
-	list_file_content();
+	int qtd_arq = list_dir();
+
+	list_file_content(qtd_arq);
 }
