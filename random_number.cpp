d46395013b965c3dc9974659898b78ae094ef23d
Duxon5 <sigajunior@hotmail.com>
2021-01-10 15:13:54 -0300

    Arquivo principal finalizado
    O arquivo random_number está finalizado.
    
    Neste commit todas as funcionalidades foram atribuidas a uma função para deixar o código mais limpo e claro.
    
    O próximo passo será atribuir as funções de gravar o resultado em arquivo txt ou csv.

--- random_number.cpp
+++ random_number.cpp
@@ -3,8 +3,36 @@
 #include <locale.h>
 #include <iostream>
 #include <time.h>
+#include <tuple>
+#include <sstream>
+
+using namespace std;
+
+bool delay = false;
+bool cls = false;
 
 void sleepcp(int milliseconds);
+
+tuple<int,int,int,int> recebeParametro();
+int ** sorteiaNumero(int qtdApostas, int qtdNumeros, int menorNum, int maiorNum);
+int ** organizaNumero(int qtdApostas, int qtdNumeros, int menorNum, int maiorNum, int **numSorteio);
+int opcaoEscolhida();
+int isInteiroPositivo();
+
+int main(){
+	setlocale (LC_ALL,"");
+
+  int qtdApostas, qtdNumeros, menorNum, maiorNum;
+  tie(qtdApostas, qtdNumeros, menorNum, maiorNum) = recebeParametro();
+
+  int **numSorteio = sorteiaNumero(qtdApostas, qtdNumeros, menorNum, maiorNum);
+
+  int **numSorteioOrganizado = organizaNumero(qtdApostas, qtdNumeros, menorNum, maiorNum, numSorteio);
+
+  int opEscolhida = opcaoEscolhida();
+
+	return 0;
+}
 
 void sleepcp(int milliseconds){
     clock_t time_end;
@@ -12,42 +40,46 @@ void sleepcp(int milliseconds){
     while (clock() < time_end){}
 }
 
-int main(){
-	setlocale (LC_ALL,"");
+tuple<int,int,int,int> recebeParametro(){
+  int qtdApostas, qtdNumeros, menorNum, maiorNum;
+	cout << "**GERADOR DE NÚMEROS QUE COM CERTEZA SERÃO SORTEADOS NA MEGA DA VIRADA**\n";
 
-	//GERA NUMEROS
-	int qtdApostas, qtdNumeros, menorNum, maiorNum;
-	printf("**GERADOR DE NÚMEROS QUE COM CERTEZA SERÃO SORTEADOS NA MEGA DA VIRADA**\n");
-	printf("Qual será a quantidade de apostas que serão feitas? ");
-	scanf("%d", &qtdApostas);
+	cout << "Qual será a quantidade de apostas que serão feitas? ";
+	qtdApostas = isInteiroPositivo();
 
-	printf("Qual será a quantidade de números que serão apostados: ");
-	scanf("%d", &qtdNumeros);
+	cout << "Qual será a quantidade de números que serão apostados: ";
+	qtdNumeros = isInteiroPositivo();
 
-	printf("\nQual o menor número possível? ");
-	scanf("%d", &menorNum);
+	cout << "\nQual o menor número possível? ";
+	menorNum = isInteiroPositivo();
 
-	printf("Qual o maior número do sorteio? ");
-	scanf("%d", &maiorNum);
+	cout << "Qual o maior número do sorteio? ";
+	maiorNum = isInteiroPositivo();
 
-	//sleepcp(2000);
+  return {qtdApostas, qtdNumeros, menorNum, maiorNum};
+}
 
-	printf("\nSORTEANDO NÚMEROS...");
-	//sleepcp(3000);
-	system("cls");
+int ** sorteiaNumero(int qtdApostas, int qtdNumeros, int menorNum, int maiorNum){
+  if(delay==true)  sleepcp(2000);
+  cout << "\nSORTEANDO NÚMEROS...";
+	if(delay==true)  sleepcp(3000);
+	if(cls==true)  system("cls");
 
 	srand (time(NULL));
-	printf("\n**NUMEROS SORTEADOS**");
-	int apostas[qtdApostas][qtdNumeros], rNum, aux, i, j, k, diferente = 1;
+  cout << "\n**NUMEROS SORTEADOS**";
+	int** apostas = new int*[qtdApostas];
+  int rNum, aux, i, j, k, diferente = 1;
 
 	for(i = 0; i < qtdApostas; i++){
-		printf("\n%d°\t", i+1);
-		//sleepcp(25);
+    cout << "\n" << i+1 << "\t";
+    apostas[i] = new int[qtdNumeros];
+
+		if(delay==true) sleepcp(25);
 		for(j = 0; j < qtdNumeros; j++){
-			//sleepcp(50);
+			if(delay==true)  sleepcp(50);
 			rNum = rand() % maiorNum + menorNum;
 
-			printf("%d	", rNum);
+      cout << rNum << "	";
 
 			apostas[i][j] = rNum;
 
@@ -65,76 +97,86 @@ int main(){
 			}
 		}
 	}
+  return apostas;
+}
 
-	//GUARDANDO OS NÚMEROS EM OUTRA VARIÁVEL, CASO NECESSÁRIO (BACKUP)
-	int apostas_bkp[i][j];
-	//printf("\n\n**REALIZANDO BACKUP...**\n");
-	for(i = 0; i < qtdApostas; i++){
-		for(j = 0; j < qtdNumeros; j++){
-			apostas_bkp[i][j] = apostas[i][j];
-		}
-	}
+int ** organizaNumero(int qtdApostas, int qtdNumeros, int menorNum, int maiorNum, int **numSorteio){
+	if(delay==true)  sleepcp(2000);
+	cout << "\n\n**APLICANDO ORDEM CRESCENTE NOS NÚMEROS SORTEADOS...**\n";
+	if(delay==true)  sleepcp(3000);
+	if(cls==true)  system("cls");
 
-	//ORGANIZA NUMEROS
-	//sleepcp(2000);
-	printf("\n\n**APLICANDO ORDEM CRESCENTE NOS NÚMEROS SORTEADOS...**\n");
-	//sleepcp(3000);
-	system("cls");
-	int cont;
+  // int** apostas = new int*[qtdApostas];
+  int aux, cont, i, j;
 
 	for(i = 0; i < qtdApostas; i++){
+    // apostas[i] = new int[qtdNumeros];
 		for(cont = 0; cont < qtdNumeros; cont++){
 			for(j = cont; j < qtdNumeros; j++){
-				if(apostas[i][cont] > apostas[i][j]){
-					aux = apostas[i][cont];
-					apostas[i][cont] = apostas[i][j];
-					apostas[i][j] = aux;
+				if(numSorteio[i][cont] > numSorteio[i][j]){
+					aux = numSorteio[i][cont];
+					numSorteio[i][cont] = numSorteio[i][j];
+					numSorteio[i][j] = aux;
 				}
 			}
 		}
 	}
 
-	printf("\n**RESULTADO**\n");
-	//sleepcp(25);
-	printf("%d apostas | %d NÚMEROs por aposta\n",qtdApostas, qtdNumeros);
-	//sleepcp(25);
-	printf("NÚMEROs entre %d e %d\n", menorNum, maiorNum);
+	cout << "\n**RESULTADO**\n";
+	if(delay==true)  sleepcp(25);
+  cout << qtdApostas << " apostas | " << qtdNumeros << " NÚMEROs por aposta\n";
+	if(delay==true)  sleepcp(25);
+  cout << "NÚMEROs entre " << menorNum << " e " << maiorNum << endl;
 	for(i = 0; i < qtdApostas; i++){
-		printf("\n%d°\t", i+1);
-		sleepcp(25);
+    cout << "\n" << i+1 << "\t";
+		if(delay==true) sleepcp(25);
 		for(j = 0; j < qtdNumeros; j++){
-			sleepcp(50);
-			printf("%d	",apostas[i][j]);
+			if(delay==true)  sleepcp(50);
+      cout << numSorteio[i][j] << "	";
 		}
 	}
+  return numSorteio;
+}
+
+int opcaoEscolhida(){
 
 	int opcao;
-	printf("\n\n------------------------------------\n");
-	printf("\nO que você deseja fazer com as apostas?");
-	printf("\n1 - Sortear novamente.");
-	printf("\n2 - Gravar o resultado em um arquivo de texto já existente.");
-	printf("\n3 - Gravar o resultado em um novo arquivo de texto.");
-	printf("\n4 - Sair\nEscolha a opção desejada:");
-	scanf("%d", &opcao);
+	cout << "\n\n------------------------------------\n";
+	cout << "\nO que você deseja fazer com as apostas?";
+	cout << "\n1 - Sortear novamente.";
+	cout << "\n2 - Gravar o resultado em um arquivo de texto já existente.";
+	cout << "\n3 - Gravar o resultado em um novo arquivo de texto.";
+	cout << "\n4 - Sair\nEscolha a opção desejada -> ";
+	cin >> opcao;
 
 	switch(opcao){
 		case 1:
-			//sleepcp(1000);
-			printf("\nO PROGRAMA SERÁ INICIADO NOVAMENTE.");
-			//sleepcp(2000);
-			system("cls");
+			if(delay==true)  sleepcp(1000);
+			cout << "\nO PROGRAMA SERÁ INICIADO NOVAMENTE.";
+			if(delay==true)  sleepcp(2000);
+			if(cls==true)  system("cls");
 			return main();
 		case 2:
 			break;
 		case 3:
 			break;
 		case 4:
-			system("cls");
-			//sleepcp(2000);
-			printf("O PROGRAMA SERÁ FINALIZADO.");
-			//sleepcp(3000);
+			if(cls==true)  system("cls");
+			if(delay==true)  sleepcp(2000);
+			cout << "\nO PROGRAMA SERÁ FINALIZADO.\n";
+			if(delay==true)  sleepcp(3000);
 			return 0;
 	}
+}
 
-	return 0;
+int isInteiroPositivo(){
+	int valor = 0;
+	cin >> valor;
+	while(cin.fail() || valor <= 0) {
+			cout << "Apenas número inteiro positivo -> ";
+			cin.clear();
+			cin.ignore(256,'\n');
+			cin >> valor;
+	}
+	return valor;
 }
